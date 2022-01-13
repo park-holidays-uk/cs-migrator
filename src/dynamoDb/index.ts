75a95607335edd9bcc981d71e6b1c312b7cb8f3a
Gordon Farquharson <godronus@yahoo.com>
2022-01-13 14:42:18 +0000

    allow dynamoDB to read and force updates

--- src/dynamoDb/index.ts
+++ src/dynamoDb/index.ts
@@ -1,5 +1,5 @@
 import AWS from 'aws-sdk';
-import { readSync } from "../dataHandler/fileCache";
+import { readSync, writeSync } from "../dataHandler/fileCache";
 
 AWS.config.update({region: 'eu-west-2'});
 const docClient = new AWS.DynamoDB.DocumentClient();
@@ -14,7 +14,7 @@ const colorLog = (color, ...restArgs) => {
   console.info(color, ...restArgs, NO_COLOUR)
 }
 
-const getUserInput = (text, defaultInput = '') => new Promise((resolve, reject) => {
+const getUserInput = (text, defaultInput = '') => new Promise<string>((resolve, reject) => {
   process.stdout.write(`${GREEN}${text} ${NO_COLOUR}`)
   process.stdin.on("data", function(data) {
     resolve(data.toString().trim() || defaultInput)
@@ -42,23 +42,65 @@ const writeToDynamoDb = async (tableName, env, type, fileName) => {
       Item: {
         id,
         ...data[id],
-        updated_at: new Date('2050/01/01').toISOString()
       }
     })
   }
   colorLog(YELLOW, `Total Items added to ${tableName}:`, itemsAdded)
 }
 
+export const arrayToKeyedObject = (arr, getKey = (item) => item.id) => arr.reduce((obj, item) => {
+  obj[getKey(item)] = item
+  return obj
+}, {});
+
+
+const getAllItems = async (params) => new Promise((resolve, reject) => {
+  docClient.scan(params, (err, data) => {
+    if (err) {
+      colorLog(RED, 'Can`t read items!', params);
+      return reject();
+    }
+    return resolve(arrayToKeyedObject(data?.Items || []));
+  });
+});
+
+const readFromDynamoDb = async (tableName, env, type, filename, forceUpdate = false) => {
+  let obj = await getAllItems({
+    TableName: tableName,
+  });
+  if (forceUpdate) {
+    obj = Object.keys(obj).reduce((acc, key) => ({
+      ...acc,
+      [key]: {
+        ...obj[key],
+        updated_at: '1960-01-01T00:00:00.000Z'
+      }
+    }), {})
+  }
+  await writeSync(env, type, filename, obj);
+  colorLog(YELLOW, `Total Items added to file ${filename}:`, Object.keys(obj).length);
+};
+
 const runImport = async () => {
-  const tableName = await getUserInput('Provide the table name: (e.g. cms_location)? ')
-  const fileName = await getUserInput('Provide the file name: (e.g. location)? ')
-  const env = await getUserInput('Provide the environment: (default: parkholidays)? ', 'parkholidays')
-  const type = await getUserInput('Provide the cacheFolder: (default: dataCache)? ', 'dataCache')
+  const tableName = await getUserInput('Provide the table name: (e.g. cms_location)? ');
+  const fileName = await getUserInput('Provide the file name: (e.g. location)? ');
+  const env = await getUserInput('Provide the environment: (default: parkholidays)? ', 'parkholidays');
+  const type = await getUserInput('Provide the cacheFolder: (default: stockCache)? ', 'stockCache');
+  const isRead = await getUserInput('Read or write (r/W)? ', 'w');
+  let forceUpdate = false;
+  if (isRead) {
+    forceUpdate = await getUserInput('Force updated_at = 1960/01/01? (y/N) ', 'n') === 'y';
+  }
 	if (!fileName || !tableName) {
     colorLog(RED, 'Please provide a valid table name & file name you wish to import!');
     return process.exit(1)
   }
-  await writeToDynamoDb(tableName, env, type, fileName)
+  if (isRead === 'r') {
+    const file = fileName.endsWith('_read') ? fileName : `${fileName}_read`
+    await readFromDynamoDb(tableName, env, type, file, forceUpdate);
+  } else if (isRead === 'w') {
+    await writeToDynamoDb(tableName, env, type, fileName);
+  }
   process.exit()
 }
 
