413c5c10dfd185dc41de61c7399f7c165fec9afc
GordonF <godronus@yahoo.com>
2023-01-12 12:16:37 +0000

    stock units

--- src/entries/stockUnit.ts
+++ src/entries/stockUnit.ts
@@ -35,12 +35,8 @@ const updateStockUnitInChild = async (
         entry: null,
       };
     }
-
-		console.log('TCL: locationUid', locationUid)
     // create stock_unit body
     let stockUnit: EntryObj = switchStackReferences(context, entry, migrationConfig.stackName);
-		console.log('TCL: stockUnit.title', stockUnit.title);
-
     stockUnit['contextual_images'] = (stockUnit['contextual_images'] ?? []).map(
       (contextualImage) => findImageRef(
         context,
@@ -49,7 +45,6 @@ const updateStockUnitInChild = async (
         contextualImage.image?.uid,
       )
     );
-		console.log('TCL: stockUnit[contextual_images]', stockUnit['contextual_images'].length)
 
     return {
       entry: scrubExistingData(stockUnit),
